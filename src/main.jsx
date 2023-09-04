026377e3503c7fe9a0879f1965454d0f76c1415b
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-04 19:36:31 +0600

    initial commit [C�

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<div className='px-4 md:px-10 lg:px-32 2xl:px-40 mx-auto'>
+<React.StrictMode>
+    <App />
+  </React.StrictMode>
+</div>
+)
