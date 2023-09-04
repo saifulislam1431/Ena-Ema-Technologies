76770497afa890b8bf90abf2dde1bc191cf53a1f
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-04 21:58:19 +0600

    Project init with page setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { HelmetProvider } from 'react-helmet-async';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router.jsx';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<div className='px-4 md:px-10 lg:px-32 2xl:px-40 mx-auto'>
-<React.StrictMode>
-    <App />
-  </React.StrictMode>
-</div>
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      </QueryClientProvider>
+      </HelmetProvider>
+    </React.StrictMode>
+  </div>
 )
