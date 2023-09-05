f362cd983450c694f220f508506936904634fa90
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-05 23:51:32 +0600

    Idea section added

--- src/Pages/Home/Idea/Idea.jsx
+++ src/Pages/Home/Idea/Idea.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import ideaImg from "../../../assets/banner/idea-01.png";
+import { Link } from 'react-router-dom';
+import { FaArrowRightLong } from "react-icons/fa6";
 const Idea = () => {
     return (
-        <div>
-            
-        </div>
+<section className='my-28 lg:w-2/3 mx-auto'>
+<div className="hero rounded-2xl" style={{backgroundImage: `url(${ideaImg})`}}>
+  <div className="hero-overlay bg-neutral bg-opacity-70 rounded-2xl"></div>
+  <div className="hero-content text-left text-neutral-content py-10 px-10">
+    <div>
+      <h1 className="mb-3 text-2xl md:text-4xl font-bold text-white brandFont">Have Any Project Idea?</h1>
+      <p className="mb-6 font-semibold text-white">Unlock the Potential of Your Offshore Project with Our Skilled Global Team. Schedule a Free Consultation Today.</p>
+      <Link to="/contact" className='myBtn inline-flex items-center gap-2'>
+      Get Stated <FaArrowRightLong className='w-6 h-6' />
+      </Link>
+    </div>
+  </div>
+</div>
+</section>
     );
 };
 
